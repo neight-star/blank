58b9a1831289919e233299f44b8437e9916596db
Night-star <2564794669@qq.com>
2024-07-20 15:39:14 +0800

    第一次提交测试

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array{
+	public static void main(String[] args){
+		int[] ns = {1,4,9,16,15};
+		for(int i = 0;i<ns.length;i++){
+			int n = ns[i];
+			System.out.println(n);
+		}	
+	}
+}
